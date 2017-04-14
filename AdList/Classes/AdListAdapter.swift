f3c10d8f1ae757957db074ab894a3ce6a09e8e1b
Sapozhnik Ivan <sapozhnik.ivan@gmail.com>
2017-04-15 01:39:01 +0200

    List adapter added

---
+++ AdList/Classes/AdListAdapter.swift
@@ -0,0 +1,13 @@
+//
+//  AdListAdapter.swift
+//  Pods
+//
+//  Created by Sapozhnik Ivan on 15/04/17.
+//
+//
+
+import UIKit
+
+class AdListAdapter: NSObject {
+
+}
---
+++ Example/Pods/Target Support Files/AdList/AdList-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AdList : NSObject
+@end
+@implementation PodsDummy_AdList
+@end
